a853ff278293abe616ba1caf475d3ddbee1755ce
ireshtharindu <ireshmeconi@gmail.com>
2024-04-20 15:09:39 +0530

    firstupdate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
+import {DashboardHomeComponent} from "./component/dashboard-home/dashboard-home.component";
+import {CustomerContextComponent} from "./component/customer-context/customer-context.component";
+import {CustomerNewComponent} from "./component/customer-context/inner/customer-new/customer-new.component";
+import {CustomerUpdateComponent} from "./component/customer-context/inner/customer-update/customer-update.component";
+import {CustomerAllComponent} from "./component/customer-context/inner/customer-all/customer-all.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', redirectTo: '/dashboard/home', pathMatch: 'full'},
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      {path: '', redirectTo: '/dashboard/home', pathMatch: 'full'},
+      {path: 'home', component: DashboardHomeComponent},
+      {
+        path:'customers',component:CustomerContextComponent,children:[
+          {path:'',redirectTo:'/dashboard/customer/new',pathMatch:'full'},
+          {path:'new',component:CustomerNewComponent},
+          {path:'update',component:CustomerUpdateComponent},
+          {path:'list',component:CustomerAllComponent},
+
+        ]
+      }
+    ]
+  }
+];

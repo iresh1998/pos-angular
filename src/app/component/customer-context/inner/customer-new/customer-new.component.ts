03a0cfad0aa0786b79312113338e51d5fd9dc17a
ireshtharindu <ireshmeconi@gmail.com>
2024-04-20 17:04:53 +0530

    customerinit

--- src/app/component/customer-context/inner/customer-new/customer-new.component.ts
+++ src/app/component/customer-context/inner/customer-new/customer-new.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-customer-new',
@@ -9,11 +10,23 @@ import {MatButton} from "@angular/material/button";
     MatInput,
     MatFormField,
     MatLabel,
-    MatButton
+    MatButton,
+    ReactiveFormsModule
   ],
   templateUrl: './customer-new.component.html',
   styleUrl: './customer-new.component.scss'
 })
 export class CustomerNewComponent {
+loading:boolean=false;
+  form=new FormGroup({
+    fullName:new FormControl('',[Validators.required]),
+    address:new FormControl('',[Validators.required]),
+    salary:new FormControl('',[Validators.required]),
+    avatar:new FormControl('',[Validators.required])
+  })
+
+  saveCustomer(){
+
+   }
 
 }

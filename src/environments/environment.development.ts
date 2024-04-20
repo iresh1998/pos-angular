a680dbb17ae1faaadba0b644a095a2f1a558b853
ireshtharindu <ireshmeconi@gmail.com>
2024-04-20 23:56:27 +0530

    insrtcomplete

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig :{
+    apiKey: "AIzaSyBKvu2i6ZpgPjr0XQbUxzrTcvjnhyp0ex0",
+    authDomain: "pos-angular-3a0e2.firebaseapp.com",
+    projectId: "pos-angular-3a0e2",
+    storageBucket: "pos-angular-3a0e2.appspot.com",
+    messagingSenderId: "450464525026",
+    appId: "1:450464525026:web:481d911c2dd13be3564cbb",
+    measurementId: "G-7XMRTM6NV4"
+  }
+};
